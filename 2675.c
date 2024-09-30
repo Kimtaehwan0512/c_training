e71ffbbf777fd12769fbb681c1050c27178ac518
Kimtaehwan0512 <saesong50@gmail.com>
2024-09-30 10:28:08 +0900

    Add files via upload

---
+++ 2675.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    
+    int n_num;
+    char n_str[20];
+    for (int i = 0;i < a;i++) {
+        scanf("%d %s", &n_num, n_str);
+        for (int j = 0;j < strlen(n_str);j++) {
+            for (int k = 0;k < n_num;k++) {
+                printf("%c", n_str[j]);
+            };
+        };
+        printf("\n");
+    };
+};
