50440cf904af6ed90dd56edca8b12ac0f2eb0123
Kimtaehwan0512 <saesong50@gmail.com>
2024-09-30 10:12:08 +0900

    Add files via upload

---
+++ 25314.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int total, now, now_n, n;
+    int res = 0;
+    scanf("%d", &total);
+    scanf("%d", &n);
+
+    for (int i = 0;i < n;i++) {
+        scanf("%d %d", &now, &now_n);
+        res = res + (now * now_n);
+    };
+
+    if (res == total) {
+        printf("Yes");
+    }
+    else {
+        printf("No");
+    };
+};
