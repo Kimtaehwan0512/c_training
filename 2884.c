4ce8ce777a7932cf59cbffb349b3ea9f08abc789
Kimtaehwan0512 <saesong50@gmail.com>
2024-09-30 09:19:58 +0900

    Add files via upload

---
+++ 2884.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (b > 44) {
+        b = b - 45;
+    } else {
+        b = b + 60 - 45;
+        a = a - 1;
+    }
+    if (a < 0) {
+        a = a + 24;
+    };
+    printf("%d %d", a, b);
+};
