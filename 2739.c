50ab801d97178f29a52243af465ece7045604b66
Kimtaehwan0512 <saesong50@gmail.com>
2024-10-02 23:12:18 +0900

    Update 2739.c

--- 2739.c
+++ 2739.c
@@ -1,8 +1,7 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int a;
 int main() {
-    int a;
     scanf("%d", &a);
     for (int i = 1;i < 10;i++) {
         printf("%d * %d = %d\n", a, i, (a * i));
