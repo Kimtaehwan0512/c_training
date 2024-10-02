a7138ba0021070144f4230f7d9111fc4307c74e7
Kimtaehwan0512 <saesong50@gmail.com>
2024-10-02 23:13:30 +0900

    Update 2675.c

--- 2675.c
+++ 2675.c
@@ -1,13 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    int a;
+int a, n_num;
+char n_str[20];
+
+int main() {    
     scanf("%d", &a);
     
-    int n_num;
-    char n_str[20];
     for (int i = 0;i < a;i++) {
         scanf("%d %s", &n_num, n_str);
         for (int j = 0;j < strlen(n_str);j++) {
