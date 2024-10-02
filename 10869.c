22e67f85bbd5866cd39b8fda36174664e7770086
Kimtaehwan0512 <saesong50@gmail.com>
2024-10-02 23:16:55 +0900

    Update 10869.c

--- 10869.c
+++ 10869.c
@@ -1,14 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-	int a, b;
+int a, b;
+
+int main() {	
 	scanf("%d %d", &a, &b);
 	printf("%d\n", (a + b));
 	printf("%d\n", (a - b));
 	printf("%d\n", (a * b));
 	printf("%d\n", (a / b));
-	printf("%d\n", (a % b));
-
-	return 0;
+	printf("%d", (a % b));
 }
