f38ee5487dab29893041d84e70585fb39522e600
Kimtaehwan0512 <saesong50@gmail.com>
2024-10-06 22:55:53 +0900

    Add files via upload

---
+++ group_5-2_5622.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+char arr[8][5] = { "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+char now[16] = "";
+int res = 0;
+
+int main() {
+    scanf("%s", now);
+
+    for (int i = 0;i < strlen(now);i++) {
+        for (int j = 0;j < 8;j++) {
+            if (strchr(arr[j], now[i])) {
+                res = res + j + 3;
+            }
+        }
+    }
+    printf("%d", res);
+}
